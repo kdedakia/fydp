2ab195515db5b115c16aad41d6f0747f850aafe3
Kishan Dedakia <kdedakia@gmail.com>
2016-01-19 13:25:32 -0500

    Fast Graphing Response, Able to Debug via Serial

--- graph/graph.py
+++ graph/graph.py
@@ -1,21 +1,12 @@
-"""
-ldr.py
-
-Display analog data from Arduino using Python (matplotlib)
-
-Author: Mahesh Venkitachalam
-Website: electronut.in
-"""
-
 import sys, serial, argparse
 import numpy as np
 from time import sleep
 from collections import deque
 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-    
+
 # plot class
 class AnalogPlot:
   # constr
@@ -44,8 +35,13 @@ class AnalogPlot:
   # update plot
   def update(self, frameNum, a0, a1):
       try:
-          line = self.ser.readline()
-          data = [float(val) for val in line.split()]
+          line = " "
+          while (line[0] != "!"):
+              print(line)
+              line = self.ser.readline()
+
+          data = [float(val) for val in line[1:].split()]
+
           # print data
           if(len(data) == 2):
               self.add(data)
@@ -53,14 +49,14 @@ class AnalogPlot:
               a1.set_data(range(self.maxLen), self.ay)
       except KeyboardInterrupt:
           print('exiting')
-      
-      return a0, 
+
+      return a0,
 
   # clean up
   def close(self):
       # close serial
       self.ser.flush()
-      self.ser.close()    
+      self.ser.close()
 
 # main() function
 def main():
@@ -71,7 +67,7 @@ def main():
 
   # parse args
   args = parser.parse_args()
-  
+
   #strPort = '/dev/tty.usbserial-A7006Yqh'
   strPort = args.port
 
@@ -83,24 +79,23 @@ def main():
   print('plotting data...')
 
   # set up animation
-  fig = plt.figure()
+  fig = plt.figure(1)
   ax = plt.axes(xlim=(0, 100), ylim=(0, 1023))
   a0, = ax.plot([], [])
   a1, = ax.plot([], [])
-  anim = animation.FuncAnimation(fig, analogPlot.update, 
-                                 fargs=(a0, a1), 
+  anim = animation.FuncAnimation(fig, analogPlot.update,
+                                 fargs=(a0, a1),
                                  interval=50)
 
   # show plot
   plt.show()
-  
+
   # clean up
   analogPlot.close()
 
   print('exiting.')
-  
+
 
 # call main
 if __name__ == '__main__':
   main()
-
